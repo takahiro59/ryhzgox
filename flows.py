d1724782c92296ae9b2dc8db912d678f6e4105e6
takahiro59 <165392316+takahiro59@users.noreply.github.com>
2024-04-13 19:45:36 +0900

    flows.py を更新

--- flows.py
+++ flows.py
@@ -3,7 +3,6 @@
 from PIL import Image
 import io, time, gzip
 import brotli
-from mitmproxy import *
 
 def response(flow):
   if "content-type" in flow.response.headers and "content-length" in flow.response.headers:
@@ -20,7 +19,8 @@ def response(flow):
          print("*** start %s ***" % (ru))
          start = time.time()
          img = Image.open(s)
-         img.save(s2, "jpeg", quality=10, optimize=True, progressive=True)
+         # img.save(s2, "jpeg", quality=10, optimize=True, progressive=True)
+         img.save(s2, "webp", quality=0)
          flow.response.content = s2.getvalue()
          ct2 = str(flow.response.headers["content-type"])
          cl2  = int(flow.response.headers["content-length"])
@@ -33,8 +33,10 @@ def response(flow):
      if (ct) [0:9] == ("image/png"):
          print("*** start %s ***" % (ru))
          start = time.time()
+         # img = Image.open(s).convert(mode='P', palette=Image.ADAPTIVE, colors=8)
+         # img.save(s2, "png", compress_level=9, bits=8)
          img = Image.open(s).convert(mode='P', palette=Image.ADAPTIVE)
-         img.save(s2, "png", compress_level=9, bits=8)
+         img.save(s2, "webp", quality=0)
          flow.response.content = s2.getvalue()
          ct2 = str(flow.response.headers["content-type"])
          cl2  = int(flow.response.headers["content-length"])
@@ -48,7 +50,20 @@ def response(flow):
          print("*** start %s ***" % (ru))
          start = time.time()
          img = Image.open(s)
-         img.save(s2, "webp", quality=10)
+         img.save(s2, "webp", quality=0)
+         flow.response.content = s2.getvalue()
+         ct2 = str(flow.response.headers["content-type"])
+         cl2  = int(flow.response.headers["content-length"])
+         i = int(cl2 /cl * 100)
+         elapsed_time = time.time() - start
+         print("*** compressed %s percent, size = %s/%s bytes, %s to %s, %s is processed, %s sec ***" % (i, cl2, cl, ct, ct2, ru, elapsed_time))
+         return
+
+     if (ct) [0:9] == ("image/gif"):
+         print("*** start %s ***" % (ru))
+         start = time.time()
+         img = Image.open(s)
+         img.save(s2, "webp", quality=0)
          flow.response.content = s2.getvalue()
          ct2 = str(flow.response.headers["content-type"])
          cl2  = int(flow.response.headers["content-length"])
